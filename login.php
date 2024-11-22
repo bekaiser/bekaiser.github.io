3a4cab0c1e613b4781e57f789aa7b1c454859afa
Bryan Kaiser <bkaiser@alum.mit.edu>
2024-11-22 13:57:49 -0700

    Delete login.php

--- login.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Simple login check (replace with real authentication logic)
-if ($_POST['username'] == 'Username' && $_POST['password'] == 'Password') {
-    header('Location: dashboard.html');
-    exit();
-} else {
-    echo 'Invalid credentials.';
-}
-?>
